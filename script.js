9aff4f4dfc28043b00d1817a3ead1432090dba3f
Arjun-1104 <Arjunrathore1104@gmail.com>
2025-01-18 23:44:48 +0530

    ui check

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 const fileAdd = document.querySelector(".fa-paperclip");
+var counter = 0;
+const arrow_left = document.querySelector(".landing_expertise .fa-arrow-left");
+const arrow_right = document.querySelector(".landing_expertise .fa-arrow-right");
 
     navLinks.forEach(link => {
       link.addEventListener('click', () => {
@@ -10,6 +13,41 @@ const fileAdd = document.querySelector(".fa-paperclip");
       });
     });
 
-    fileAdd.addEventListener('click',()=> {
+    // fileAdd.addEventListener('click',()=> {
         
+    // })
+
+
+    const carousel_item = Array.from(document.querySelectorAll(".carousel_item .item"))
+    console.log(carousel_item)
+
+    carousel_item.forEach((item,index)=>{
+      item.style.left = `${index*33}%`
     })
+
+    const prev = () =>{
+      counter--;
+      if(counter<0){
+        counter = carousel_item.length-3;
+      }
+      arrow_left.style.color = 'rgba(247, 118, 31, 1)';
+      arrow_right.style.color = 'rgba(17, 19, 30, 0.2)';
+      slidebox();
+    }
+
+    const next = () =>{
+      counter++;
+      if(counter>carousel_item.length-3){
+        counter = 0;
+      }
+      arrow_right.style.color = 'rgba(247, 118, 31, 1)';
+      arrow_left.style.color = 'rgba(17, 19, 30, 0.2)';
+
+      slidebox();
+    }
+
+    const slidebox = () => {
+      carousel_item.forEach((item)=>{
+        item.style.transform = `translateX(-${counter*100}%)`
+      })
+    }
