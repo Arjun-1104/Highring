5772ce252af10bee13867603efe8afdec3358074
Arjun-1104 <Arjunrathore1104@gmail.com>
2025-01-19 11:30:29 +0530

    some js add

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const fileAdd = document.querySelector(".fa-paperclip");
 var counter = 0;
 const arrow_left = document.querySelector(".landing_expertise .fa-arrow-left");
 const arrow_right = document.querySelector(".landing_expertise .fa-arrow-right");
-
+const carousel_item = Array.from(document.querySelectorAll(".carousel_item .item"))
+let formSubmitCount = 1;
     navLinks.forEach(link => {
       link.addEventListener('click', () => {
         // Remove 'active' class from all links
@@ -18,8 +19,7 @@ const arrow_right = document.querySelector(".landing_expertise .fa-arrow-right")
     // })
 
 
-    const carousel_item = Array.from(document.querySelectorAll(".carousel_item .item"))
-    console.log(carousel_item)
+    // console.log(carousel_item)
 
     carousel_item.forEach((item,index)=>{
       item.style.left = `${index*33}%`
@@ -51,3 +51,44 @@ const arrow_right = document.querySelector(".landing_expertise .fa-arrow-right")
         item.style.transform = `translateX(-${counter*100}%)`
       })
     }
+
+
+    // form submit
+    const form = document.getElementById("getInTouch");
+    
+    let username = form.username.value
+    let useremail = form.useremail.value
+    let usertimeSlot = form.usertimeSlot.value
+    let userDepartment = form.userDepartment.value
+    let userMessage = form.userMessage.value
+
+    let userSubmit = ["name", "email", "timeSlot", "department", "message","fileName"]
+    // let userBigData = {}
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let userData = {};
+        Array.from(e.target).forEach((input,index) => {
+          if(index === 0 || index === 1 || index === 2 || index === 3 || index === 4){
+            console.log(input.value)
+            userData = {...userData, [userSubmit[index]]:input.value}
+          } 
+          if(index === 5){
+            const fileInput = document.getElementById("fileInput");
+            const file = fileInput.files[0];
+
+            if(file){
+              userData = {...userData, [userSubmit[index]]:file.name}
+            }
+          }
+        })
+        // console.log(userData)
+        localStorage.setItem(formSubmitCount,JSON.stringify(userData))
+        // userBigData = {...userBigData,[formSubmitCount]:userData}
+        formSubmitCount++;
+        // console.log(userBigData)
+    });
+    
+  
+
+
+
